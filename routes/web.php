4156a8217822b63a9bb691b451ede20ff78faba0
gereiz <georgie.reis@outlook.com>
2021-11-06 17:14:56 -0300

    Criação das functions no COntroller
    Criação do controler e ajustes no banco de dados e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +11,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "ContatoController@index");
+Route::get('contato/json', "ContatoController@indexjson");
+
+/**
+ * Categoria de produtos
+ */
+Route::resource("contato", "ContatoController");
 
 Auth::routes();
 
